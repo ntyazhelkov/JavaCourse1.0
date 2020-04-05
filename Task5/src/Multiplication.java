373585b2aace65e21fae75f96b934ac4dae60497
ntyazhelkov <pronger95@gmail.com>
2020-04-05 19:24:26 +0300

    Задание №5, Ввод числа и вывод таблицы умножения для этого числа от 1 до 10

---
+++ Task5/src/Multiplication.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Multiplication {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int a = scanner.nextInt();
+        System.out.print(a + "\n" +a*2 + "\n" +a*3+"\n"+a*4+"\n"+a*5+"\n"+a*6+"\n"+a*7+"\n"+a*8+"\n"+a*9+"\n"+a*10);
+    }
+}
