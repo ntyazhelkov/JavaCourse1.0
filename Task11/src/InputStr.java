43f13fcdb479c96ecdb3f68b2df9f08f5cc8b4f1
ntyazhelkov <pronger95@gmail.com>
2020-04-12 19:54:59 +0300

    Задание №11 - Ввод строки с клавиатуры, запись в переменную и вывод на экран.

---
+++ Task11/src/InputStr.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class InputStr {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(str);
+    }
+}
