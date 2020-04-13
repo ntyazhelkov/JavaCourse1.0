b017d3087683de5e4f94f5c6418c67a0a00e599d
ntyazhelkov <pronger95@gmail.com>
2020-04-13 20:12:51 +0300

    Задание №10.1 - Добавлен ввод размерности матрицы с клавиатуры.

--- Task10/src/Matrix.java
+++ Task10/src/Matrix.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class Matrix {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int mat[][] = new int[3][4];
+        System.out.println("Введите количество строк в матрице");
+        int x = scanner.nextInt();
+        System.out.println("Введите количество стоблцев в матрице");
+        int y = scanner.nextInt();
+
+        int mat[][] = new int[x][y];
 
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 4; j++){
