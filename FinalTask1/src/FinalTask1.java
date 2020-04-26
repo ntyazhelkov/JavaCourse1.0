57197064ccf0cac1e0045f395c1abb22f9107b6d
ntyazhelkov <pronger95@gmail.com>
2020-04-27 01:09:08 +0300

    Итоговое задание №1.1 - Добавил проверку ввода.

--- FinalTask1/src/FinalTask1.java
+++ FinalTask1/src/FinalTask1.java
@@ -3,8 +3,25 @@ import java.util.Scanner;
 public class FinalTask1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите двоичное число:");
-        String bin = scanner.nextLine();
+        String bin = "";
+        boolean zet = false;
+        do {
+            System.out.println("Введите двоичное число:");
+            bin = scanner.nextLine();
+            for (int i = 0; i < bin.length(); i++) {
+                if ((bin.charAt(i) - '0') == 1) {
+                    zet = false;
+                }
+                else if ((bin.charAt(i) - '0') == 0 )
+                    zet = false;
+                else {
+                    zet = true;
+                    System.out.println("Ошибка ввода! Это не двоичное число");
+                    break;
+                }
+            }
+        }
+        while(zet);
         int n = 0;
         for (int i = bin.length() - 1, j = 1; i >= 0; i--, j *= 2) {
             n += (bin.charAt(i) - '0') * j;
