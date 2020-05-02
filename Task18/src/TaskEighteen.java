8b46430874cde48c81e655f9ce8bc5754d5f10cf
ntyazhelkov <pronger95@gmail.com>
2020-05-02 18:12:57 +0300

    Задание №18.1 - Внес исправления, можем ввести и записать в файл столько строк, сколько их было в файле изначально.

--- Task18/src/TaskEighteen.java
+++ Task18/src/TaskEighteen.java
@@ -3,24 +3,29 @@ import java.io.*;
 public class TaskEighteen {
     public static void main(String[] args) {
         String s, str;
+        int i = 0, j = 0;
         BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
         try (BufferedReader br2 = new BufferedReader(new FileReader("C:\\Users\\user\\Documents\\test.txt"));
              ) {
-            while ((s = br2.readLine()) != null) {
+           while ((s = br2.readLine()) != null) {
                 System.out.println(s);
-                try (FileWriter fw = new FileWriter("C:\\Users\\user\\Documents\\test.txt")) {
-                    do {
-                        str = null;
-                        str = br1.readLine();
-                        if (str.length() == s.length()) {fw.write(str); break; }
-                        else if ((str.length() > s.length()) || (str.length() < s.length())) continue;
-                    } while (str.length() != s.length());
-                } catch (IOException exc) {
-                    System.out.println("Ошибка ввода-вывода: " + exc);
+                i++;
+            }
+            System.out.println(i);
+
+            try (FileWriter fw = new FileWriter("C:\\Users\\user\\Documents\\test.txt")) {
+                while (j < i) {
+                    str = br1.readLine();
+                    fw.write(str + "\n");
+                    j++;
                 }
+            } catch (IOException exc) {
+                System.out.println("Ошибка ввода-вывода: " + exc);
             }
         } catch (IOException exc) {
             System.out.println("Ошибка ввода-вывода: " + exc);
         }
+
+
     }
 }
